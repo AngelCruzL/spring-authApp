9216a3c5f937b4ea7b4ba7d2177d79a3a60ab279
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-02 09:05:40 -0600

    Add validation on create new user handler

--- src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
+++ src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
@@ -1,9 +1,12 @@
 package dev.angelcruzl.authsystem.controller;
 
 import dev.angelcruzl.authsystem.dto.UserDto;
+import dev.angelcruzl.authsystem.entity.User;
 import dev.angelcruzl.authsystem.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +33,25 @@ public class AuthController {
   }
 
   @PostMapping("/register/save")
-  public String handleRegisterForm(@ModelAttribute("user") UserDto userDto) {
+  public String handleRegisterForm(
+      @Valid @ModelAttribute("user") UserDto userDto,
+      BindingResult result,
+      Model model
+  ) {
+    User existingUser = userService.findUserByEmail(userDto.getEmail());
+    if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+      result.rejectValue(
+          "email",
+          null,
+          "There is already an account registered with that email"
+      );
+    }
+
+    if (result.hasErrors()) {
+      model.addAttribute("user", userDto);
+      return "/register";
+    }
+
     userService.saveUser(userDto);
 
     return "redirect:/register?success";
--- src/main/java/dev/angelcruzl/authsystem/service/UserService.java
+++ src/main/java/dev/angelcruzl/authsystem/service/UserService.java
@@ -1,7 +1,10 @@
 package dev.angelcruzl.authsystem.service;
 
 import dev.angelcruzl.authsystem.dto.UserDto;
+import dev.angelcruzl.authsystem.entity.User;
 
 public interface UserService {
   void saveUser(UserDto userDto);
+
+  User findUserByEmail(String email);
 }
