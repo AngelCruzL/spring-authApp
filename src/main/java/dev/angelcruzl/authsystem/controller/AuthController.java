8fbcf2eddf06b4cb611f3ce236ab1d518a939936
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-02 08:41:49 -0600

    Add functionality to create a new user

--- src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
+++ src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
@@ -1,12 +1,21 @@
 package dev.angelcruzl.authsystem.controller;
 
 import dev.angelcruzl.authsystem.dto.UserDto;
+import dev.angelcruzl.authsystem.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+  private final UserService userService;
+
+  public AuthController(UserService userService) {
+    this.userService = userService;
+  }
+
   @GetMapping("/")
   public String index() {
     return "index";
@@ -18,5 +27,12 @@ public class AuthController {
     model.addAttribute("user", user);
 
     return "register";
+  }
+
+  @PostMapping("/register/save")
+  public String handleRegisterForm(@ModelAttribute("user") UserDto userDto) {
+    userService.saveUser(userDto);
+
+    return "redirect:/register?success";
   }
 }
