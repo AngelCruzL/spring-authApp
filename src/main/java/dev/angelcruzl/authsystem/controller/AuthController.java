aa9ce2764b5f2e08ecbd2cadc335c8428163b0bf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-02 08:18:18 -0600

    Add AuthController functionality to show a register page

--- src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
+++ src/main/java/dev/angelcruzl/authsystem/controller/AuthController.java
@@ -1,6 +1,8 @@
 package dev.angelcruzl.authsystem.controller;
 
+import dev.angelcruzl.authsystem.dto.UserDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +10,13 @@ public class AuthController {
   @GetMapping("/")
   public String index() {
     return "index";
+  }
+
+  @GetMapping("/register")
+  public String showRegistrationForm(Model model) {
+    UserDto user = new UserDto();
+    model.addAttribute("user", user);
+
+    return "register";
   }
 }
