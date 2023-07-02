afd5284e0c445761b27e75f705f690ced95a2735
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-07-02 12:50:44 -0600

    Fix login page form to log in with email instead of username

--- src/main/java/dev/angelcruzl/authsystem/security/CustomUserDetailsService.java
+++ src/main/java/dev/angelcruzl/authsystem/security/CustomUserDetailsService.java
@@ -19,8 +19,8 @@ public class CustomUserDetailsService implements UserDetailsService {
   }
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByEmail(username);
+  public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(usernameOrEmail);
     if (user != null) {
       return new org.springframework.security.core.userdetails.User(
           user.getEmail(),
